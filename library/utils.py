61c446bee16b6cc7bfd0b99bfb43027942a7ac43
alina-arsenic <85684263+alina-arsenic@users.noreply.github.com>
2021-06-13 00:59:17 +0300

    Update utils.py

--- library/utils.py
+++ library/utils.py
@@ -1,6 +1,18 @@
+import pandas as pd
+
+
 def get_types_list(dataframe):
-    pass
+    for name in dataframe.columns:
+        try:
+            if dataframe[name][0].split(':') == 3:
+                break
+        except AttributeError:
+            pass
 
-
-def split_categories(dataframe):
-    pass
+    time = []
+    dataframe[name].apply(lambda x: x.split(':')).apply(
+        lambda x: time.append(pd.Timedelta(days=int(x[0]), hours=int(x[1]), minutes=int(x[2]))))
+    dataframe[name] = pd.Series(time)
+    types = dataframe.dtypes
+    
+    return types
