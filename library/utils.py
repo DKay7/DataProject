446ab958e5bdfd036b520adc2986ee9faec31927
alina-arsenic <85684263+alina-arsenic@users.noreply.github.com>
2021-06-13 23:52:36 +0300

    Update utils.py

--- library/utils.py
+++ library/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def get_types_list(dataframe):
     flag = False
-    
+
     for name in dataframe.columns:
         try:
             if len(dataframe[name][0].split(':')) == 3:
@@ -11,12 +11,13 @@ def get_types_list(dataframe):
                 break
         except AttributeError:
             pass
-    
+
     if flag:
         time = []
         dataframe[name].apply(lambda x: x.split(':')).apply(
             lambda x: time.append(pd.Timedelta(days=int(x[0]), hours=int(x[1]), minutes=int(x[2]))))
         dataframe[name] = pd.Series(time)
+        dataframe.rename(columns={name: 'Total Flight Time'}, inplace=True)
 
     types = dataframe.dtypes
 
