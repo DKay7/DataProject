87cc511e5aa2d4d23e45de444357fd2c2de2444a
alina-arsenic <85684263+alina-arsenic@users.noreply.github.com>
2021-06-13 15:01:13 +0300

    Update utils.py

--- library/utils.py
+++ library/utils.py
@@ -2,17 +2,22 @@ import pandas as pd
 
 
 def get_types_list(dataframe):
+    flag = false
+
     for name in dataframe.columns:
         try:
             if dataframe[name][0].split(':') == 3:
+                flag = true
                 break
         except AttributeError:
             pass
 
-    time = []
-    dataframe[name].apply(lambda x: x.split(':')).apply(
-        lambda x: time.append(pd.Timedelta(days=int(x[0]), hours=int(x[1]), minutes=int(x[2]))))
-    dataframe[name] = pd.Series(time)
+    if flag:
+        time = []
+        dataframe[name].apply(lambda x: x.split(':')).apply(
+            lambda x: time.append(pd.Timedelta(days=int(x[0]), hours=int(x[1]), minutes=int(x[2]))))
+        dataframe[name] = pd.Series(time)
+
     types = dataframe.dtypes
-    
+
     return types
